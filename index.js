cd98162f535502be3be2b5aaac2bf618ab28d434
Jonatan Ferrari <jonaaf32@gmail.com>
2022-11-27 16:53:49 -0300

    Comenzando Proyecto...

---
+++ index.js
@@ -0,0 +1,114 @@
+
+/* let boton1 = document.getElementById ("botonUno");
+let img1 = document.getElementById ("imagenUno")
+function correcto1() {
+        botonUno.classList.add("rojo");
+        imagenUno.classList.add ("rojo1");
+        alert ("correcto")
+}
+boton1.addEventListener("click", correcto1);
+imagenUno.addEventListener ("click", correcto1)
+ */
+
+
+
+let boton2 = document.querySelector (".clicDos");
+let img2 = document.querySelector (".clicDosImg");
+
+let correcto2 = () => {
+    boton2.classList.add("azul"); 
+    img2.style.display = 'flex' 
+}
+
+boton2.addEventListener("click", correcto2);
+img2.addEventListener("click", correcto2);
+
+
+let boton3 = document.querySelector (".clicTres");
+let img3 = document.querySelector (".clicTresImg");
+
+let correcto3 = () => {
+    boton3.classList.add("verde"); 
+    img3.style.display = 'flex' 
+}
+boton3.addEventListener("click", correcto3);
+img3.addEventListener("click", correcto3);
+
+let boton4 = document.querySelector (".clicCuatro");
+let img4 = document.querySelector (".clicCuatroImg");
+
+let correcto4 = () => {
+    boton4.classList.add("rosa"); 
+    img4.style.display = 'flex' 
+}
+boton4.addEventListener("click", correcto4);
+img4.addEventListener("click", correcto4);
+
+let boton5 = document.querySelector (".clicCinco");
+let img5 = document.querySelector (".clicCincoImg");
+
+let correcto5 = () => {
+    boton5.classList.add("flor"); 
+    img5.style.display = 'flex' 
+}
+boton5.addEventListener("click", correcto5);
+img5.addEventListener("click", correcto5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let boton1 = 
+document.getElementById ("botonUno");
+document.getElementById ("imagenUno")
+document.getElementById ("botonDos");
+document.getElementById ("imagenDos");
+document.getElementById ("botonTres");
+document.getElementById ("imagenTres");
+document.getElementById ("botonCuatro");
+document.getElementById ("imagenCuatro");
+document.getElementById ("botonCinco");
+document.getElementById ("imagenCinco");
+function correcto1() {
+if ("click") {
+        botonUno.classList.add("rojo");
+        imagenUno.classList.add ("rojo1");
+	botonDos.classList.add("azul");
+        imagenDos.classList.add ("azul1"); 
+	botonTres.classList.add("verde")
+        imagenTres.classList.add ("verde1");
+	botonCuatro.classList.add ("rosa");
+        imagenCuatro.classList.add ("rosa1")
+	 botonCinco.classList.add ("flor");
+        imagenCinco.classList.add ("flor1");
+
+        alert ("correcto")
+}
+}
+botonUno.addEventListener("click", correcto1);
+imagenUno.addEventListener ("click", correcto1);
+botonDos.addEventListener ("click", correcto1);
+imagenDos.addEventListener ("click", correcto1);
+botonTres.addEventListener ("click", correcto1);
+imagenTres.addEventListener ("click", correcto1);
+botonCuatro.addEventListener ("click", correcto1);
+imagenCuatro.addEventListener ("click", correcto1);
+botonCinco.addEventListener ("click", correcto1);
+imagenCinco.addEventListener ("click", correcto1); */
